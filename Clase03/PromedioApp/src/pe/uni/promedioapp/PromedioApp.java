d9492be9c879f3ce345cca9c824f55c17d67f112
Gustavo coronel <gcoronelc@gmail.com>
2020-01-15 21:23:08 -0500

    Clase03

--- Clase03/PromedioApp/src/pe/uni/promedioapp/PromedioApp.java
+++ Clase03/PromedioApp/src/pe/uni/promedioapp/PromedioApp.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pe.uni.promedioapp;
 
-/**
- *
- * @author Alumno
- */
+import pe.uni.promedioapp.view.AlumnoView;
+
 public class PromedioApp {
 
-  /**
-   * @param args the command line arguments
-   */
   public static void main(String[] args) {
-    // TODO code application logic here
+    AlumnoView.main(args);
   }
   
 }
